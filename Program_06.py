bde872f04b0d1c85e2446f1d46e7354969341f8d
UsmanWebDev <usmanwebdevelopmentwithreactjs@gmail.com>
2024-10-12 08:14:39 +0000

    Create a dictionary where the keys are the names of 5 different countries and the values are their capitals. Write a program to display all the countries and their capitals.

---
+++ Program_06.py
@@ -0,0 +1,10 @@
+
+countries_and_capitals = {
+    "Pakistan": "Islamabad",
+    "china":"Beijing",
+    "India": "New Delhi",
+  
+}
+
+for country, capital in countries_and_capitals.items():
+    print(f"{country}: {capital}")
