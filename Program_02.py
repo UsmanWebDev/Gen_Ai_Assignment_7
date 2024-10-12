0d6d677ac95b730d45013e850b25a52c05581b70
UsmanWebDev <usmanwebdevelopmentwithreactjs@gmail.com>
2024-10-12 07:54:33 +0000

    Given a list of fruits: ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'mango'], write a program to:
    Access the first, middle, and last element of the list.
    Change the second element to 'blueberry'.

---
+++ Program_02.py
@@ -0,0 +1,16 @@
+# List of fruits
+fruits = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'mango']
+
+
+first_element = fruits[0]
+middle_element = fruits[len(fruits) // 2]  
+last_element = fruits[-1]
+
+
+fruits[1] = 'blueberry'
+
+
+print("First element:", first_element)
+print("Middle element:", middle_element)
+print("Last element:", last_element)
+print("Updated list:", fruits)
