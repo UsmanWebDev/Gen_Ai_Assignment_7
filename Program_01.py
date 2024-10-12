dadf1e57e8b80217ae0ca8077762659cb46fe914
UsmanWebDev <usmanwebdevelopmentwithreactjs@gmail.com>
2024-10-12 07:50:55 +0000

    Write a Python program to create a list of 5 numbers. Add an element to the list, remove one element, and find the maximum and minimum number in the list.

---
+++ Program_01.py
@@ -0,0 +1,16 @@
+
+numbers = [10, 20, 30, 40, 50]
+
+
+numbers.append(60)
+
+
+numbers.remove(20)
+
+
+max_number = max(numbers)
+min_number = min(numbers)
+
+print("Updated list:", numbers)
+print("Maximum number:", max_number)
+print("Minimum number:", min_number)
